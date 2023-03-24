5a9180b078e58480dc522df0153c6186bdf92e88
Jeeva Kumar <jeevakba2024@email.iimcal.ac.in>
2023-03-24 16:28:56 +0530

    Optimised but dont know how

--- assignment-2-22BM6JP52.py
+++ assignment-2-22BM6JP52.py
@@ -1,31 +1,56 @@
 import numpy as np
 import sys 
 import time
-
+import random
 
 class DisjointSet:
-	def __init__(self, n):
-		self.rank = [1] * n
-		self.parent = [i for i in range(n)]
-		self.sets_count = n
-
-	def find(self, x):
-		if (self.parent[x] != x):
-			self.parent[x] = self.find(self.parent[x])
-		return self.parent[x]
-
-	def union(self, x, y):
-		xset = self.find(x)
-		yset = self.find(y)
-
-		if xset == yset:
-			return
-
-		else:
-			self.parent[yset] = xset
-			self.rank[xset] = self.rank[xset] + 1	
-			self.sets_count = self.sets_count - 1 
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+        self.sets_count = n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return
+        self.sets_count -= 1
+        if self.rank[px] < self.rank[py]:
+            self.parent[px] = py
+        elif self.rank[px] > self.rank[py]:
+            self.parent[py] = px
+        else:
+            self.parent[py] = px
+            self.rank[px] += 1
 		
+def gen_graph(edges):
+	graph = {}
+	for edge in edges:
+		if edge[0] not in graph:
+			graph[edge[0]] = []
+		if edge[1] not in graph:
+			graph[edge[1]] = []
+		graph[edge[0]].append(edge[1])
+		graph[edge[1]].append(edge[0])
+	return graph
+    
+def karger_min_cut(graph):
+    n = len(graph)
+    uf = DisjointSet(n)
+    while uf.sets_count > 2:
+        u = random.randint(0, n-1)
+        v = random.choice(graph[u])
+        uf.union(u, v)
+    cut_size = 0
+    for u in range(n):
+        for v in graph[u]:
+            if uf.find(u) != uf.find(v):
+                cut_size += 1
+    return cut_size // 2  
 
 if __name__ == "__main__":
 
@@ -33,14 +58,8 @@ if __name__ == "__main__":
 		exit("Number of arg didn't match")
 
 	start = time.time()
-	print(start)
 	edges = np.loadtxt(sys.argv[1], dtype=int, delimiter = ' ')
-	graph = DisjointSet(edges.max()+1)
-	while (graph.sets_count > 3):
-		edge_index = np.random.choice(edges.shape[0], replace=False)
-		random_edge = edges[edge_index,:]
-		graph.union(random_edge[0], random_edge[1])
-		if(graph.sets_count%1000 == 0):
-			print('Number of sets = {}: {}'.format(graph.sets_count, time.time() - start))
+	graph = gen_graph(edges)
+	print("Graph generated:{}".format(time.time() - start))
+	print(karger_min_cut(graph.graph))
 	print(time.time() - start)
-	print(graph.parent)
