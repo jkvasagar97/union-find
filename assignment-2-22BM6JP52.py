27048156c9f347e5f656bb91d7e5d59c183e68a6
Jeeva Kumar <jeevakba2024@email.iimcal.ac.in>
2023-03-29 12:54:34 +0530

    Diff data

--- assignment-2-22BM6JP52.py
+++ assignment-2-22BM6JP52.py
@@ -7,6 +7,7 @@ class Graph:
     def __init__(self, edges):
         self.adj_list = {}
         self.clusters = {}
+        self.visited = {}
         for u, v in edges:
             self.add_edge(u, v)
             
@@ -14,20 +15,25 @@ class Graph:
         if u not in self.adj_list:
             self.adj_list[u] = []
             self.clusters[u] = {u}
+            self.visited[u] = False
         if v not in self.adj_list:
             self.adj_list[v] = []
             self.clusters[v] = {v}
+            self.visited[v] = False
         self.adj_list[u].append(v)
         self.adj_list[v].append(u)
         
     def min_cut(self):
         while len(self.clusters) > 2:
-            u, v = self.choose_random_edge()
-            self.contract(u, v)
+            u, v = self.choose_random_edge() # choosing a random edge
+            self.contract(u, v) # contract the randomly connected edge 
         min_cut = 0
-        for i in self.adj_list:
+        for i in self.adj_list: 
+            #adj list only consists of connections to the other set
+            #adding all the edges and dividing them by 2 will the number 
+            #of edges connecting the 2 sets
             min_cut += len(self.adj_list[i])
-        return min_cut // 2, list(self.clusters.values())
+        return min_cut // 2
         
     def choose_random_edge(self):
         u = random.choice(list(self.clusters.keys()))
@@ -35,21 +41,53 @@ class Graph:
         return u, v
         
     def contract(self, u, v):
-        # merge v into u
+        '''
+        Merges two nodes into one node by updating the 
+        cluster/ adj list and deleting the node from adj list/ cluster 
+        '''
         self.clusters[u].update(self.clusters[v])
         del self.clusters[v]
-        for i in self.adj_list[v]:
-            if i != u:
+        for i in self.adj_list[v]: #for each node in niebhourhood 
+            if i != u: #extend the list when the nodes are not equal
                 self.adj_list[u].append(i)
                 self.adj_list[i].append(u)
             self.adj_list[i].remove(v)
-        del self.adj_list[v]
+        del self.adj_list[v] # deleting the merged node from adj list
+
+    def count_components(self):
+        """
+        Returns the number of connected components
+        Used to check if the graph has only one connected component
+        """
+        count = 0
+        for u in self.adj_list:
+            if not self.visited[u]:
+                count += 1
+                self.visited[u] = True
+                stack = [u]
+                while stack:
+                    u = stack.pop()
+                    for v in self.adj_list[u]:
+                        if not self.visited[v]:
+                            self.visited[v] = True
+                            self.clusters[u].update(self.clusters[v])
+                            stack.append(v)
+            return count
+
+    def format_print(self):
+        for clno, cluster in enumerate(self.clusters.values()):
+            for node in cluster:
+                print("{} {}".format(node, clno + 1))
+
 
 if __name__ == '__main__':
-    start = time.time()
+    #start = time.time()
     edges = np.loadtxt(sys.argv[1], dtype=int, delimiter = ' ')
     g = Graph(edges)
-    min_cut, clusters = g.min_cut()
-    print('The minimum cut is:', min_cut)
-    print('The clusters are:', clusters)
-    print('Time taken: ', time.time() - start)
+    #print("Graph generated: {}".format(time.time() - start))
+    #print(g.count_components())
+    min_cut = g.min_cut()
+    #print("Min cut generated: {}".format(time.time() - start))
+    print(min_cut)
+    g.format_print()
+    #print('Time taken: ', time.time() - start)
